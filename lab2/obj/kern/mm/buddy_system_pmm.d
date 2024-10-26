df8e402c95c24662a1ad0cb8321ff8157605fea9
Lllsir0202 <q2696127622@outlook.com>
2024-10-26 10:38:21 +0800

    请注意lab2 的challenge在分支to_the_moon中

--- lab2/obj/kern/mm/buddy_system_pmm.d
+++
@@ -1,5 +0,0 @@
-obj/kern/mm/buddy_system_pmm.o obj/kern/mm/buddy_system_pmm.d: \
- kern/mm/buddy_system_pmm.c kern/mm/memlayout.h libs/defs.h libs/atomic.h \
- libs/list.h kern/mm/pmm.h kern/debug/assert.h kern/mm/memlayout.h \
- kern/mm/mmu.h libs/riscv.h libs/string.h kern/mm/buddy_system_pmm.h \
- libs/stdio.h libs/stdarg.h
